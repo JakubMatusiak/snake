7c4193e3959c1bd1249b8cb5bc0a9c4f16cba360
JakubMatusiak <44224610+JakubMatusiak@users.noreply.github.com>
2018-11-22 14:17:37 +0100

    Update main.cpp

--- include -GL/main.cpp
+++ include -GL/main.cpp
@@ -8,6 +8,12 @@
 #define ROWS 40
 #define FPS 10
 
+
+static int window;
+static int menu_id;
+static int submenu_id;
+static int value = 0;
+
 extern short sDirection;
 bool gameOver=false;
 
@@ -15,8 +21,14 @@ int score=0;
 
 void timer_callback(int);
 void display_callback();
+void display_callback2();
 void reshape_callback(int,int);
 void keyboard_callback(int,int,int);
+void keyboard_callback2(int,int,int);
+void display_menu();
+void createMenu();
+
+
 
 void init()
 {
@@ -29,14 +41,17 @@ int main(int argc, char **argv)
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
     glutInitWindowSize(500,500);
-    glutCreateWindow("moze");
+    window = glutCreateWindow("snake");
+    createMenu();
+    glClearColor(0.0,0.0,0.0,0.0);
+    glutDisplayFunc(display_menu);
     glutDisplayFunc(display_callback);
     glutReshapeFunc(reshape_callback);
     glutTimerFunc(0,timer_callback,0);
     glutSpecialFunc(keyboard_callback);
     init();
     glutMainLoop();
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 void display_callback()
@@ -44,6 +59,7 @@ void display_callback()
     glClear(GL_COLOR_BUFFER_BIT);
     drawGrid();
     drawSnake();
+    drawSnake2();
     drawFood();
     glutSwapBuffers();
     if(gameOver)
@@ -55,8 +71,15 @@ void display_callback()
         MessageBox(NULL,text,"Koniec Gry",0);
         exit(0);
     }
+display_callback2();
 }
 
+void display_callback2()
+{
+
+   drawSnake2();
+
+}
 void reshape_callback(int w, int h)
 {
     glViewport(0,0,(GLsizei)w,(GLsizei)h);
@@ -94,6 +117,63 @@ void keyboard_callback(int key, int, int)
             break;
     }
 }
+
+void keyboard_callback2(int key, int, int)
+{
+ switch(key)
+    {
+    case GLUT_KEY_F1:
+            if(sDirection!=DOWN)
+                sDirection=UP;
+            break;
+     case GLUT_KEY_F2:
+             if(sDirection!=UP)
+                sDirection=DOWN;
+            break;
+      case GLUT_KEY_F4:
+             if(sDirection!=LEFT)
+                sDirection=RIGHT;
+            break;
+      case GLUT_KEY_F3:
+             if(sDirection!=RIGHT)
+                sDirection=LEFT;
+            break;
+    }
+}
+
+void menu(int num){
+  if(num == 0){
+    glutDestroyWindow(window);
+    exit(0);
+  }else{
+    value = num;
+  }
+  glutPostRedisplay();
+
+} void createMenu(void){
+    menu_id = glutCreateMenu(menu);
+    glutAddMenuEntry("1", 1);
+    glutAddSubMenu("2", 2);
+    glutAddMenuEntry("Quit", 0);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+void display_menu(void){
+  glClear(GL_COLOR_BUFFER_BIT);   if(value == 1){
+    return; //glutPostRedisplay();
+  }else if(value == 1){
+    glPushMatrix();
+////
+    glPopMatrix();
+  }else if(value == 2){
+    glPushMatrix();
+////
+    glPopMatrix();
+
+  }
+  glFlush();
+}
+
 
 
 
