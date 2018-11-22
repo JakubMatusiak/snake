a5649811296a951ad2048122293d2a5ec081162b
JakubMatusiak <44224610+JakubMatusiak@users.noreply.github.com>
2018-11-22 14:18:12 +0100

    Update game.h

--- include -GL/game.h
+++ include -GL/game.h
@@ -6,11 +6,18 @@
 #define RIGHT 2
 #define LEFT -2
 
+#define F1 1
+#define F2 -1
+#define F4 2
+#define F3 -2
+
+
 #define MAX 60
 
 void initGrid(int,int);
 void drawGrid();
 void drawSnake();
+void drawSnake2();
 void drawFood();
 void random(int&,int&);
 
